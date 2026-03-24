14f4212c2e3040608c990952f7d6277cdbbec4d9
StarryPurple <starrypurple@163.com>
2026-03-24 13:46:16 +0800

    docs: update the descriptions of priority queue and its member functions

--- priority_queue/include/priority_queue.hpp
+++ priority_queue/include/priority_queue.hpp
@@ -1,7 +1,7 @@
 #ifndef SJTU_PRIORITY_QUEUE_HPP
 #define SJTU_PRIORITY_QUEUE_HPP
 
-#include <cmath>       // maybe you need it
+#include <cmath>       // in case you need it
 #include <cstddef>     // for size_t
 #include <functional>  // for std::less
 
@@ -9,54 +9,62 @@
 
 namespace sjtu {
 
+/**
+ * @brief A container automatically sorting its contents, similar to
+ * std::priority_queue but with extra functionalities.
+ *
+ * The extra functionalities are:
+ * - Merge two priority queues into one (with good time complexity).
+ * - Clear all elements in the queue.
+ * - Limited exception safety for some operations (e.g. push, pop, top, merge)
+ * when the comparator throws exceptions from `Compare` only.
+ *
+ * This @priority_queue does not support passing an underlying container as a template parameter.
+ * Also, it does not support passing a comparator object as a constructor argument.
+ *
+ */
 template <class T, class Compare = std::less<T>>
 class priority_queue {
-public:
+   public:
     priority_queue();
     priority_queue(const priority_queue&);
     ~priority_queue();
 
     priority_queue& operator=(const priority_queue&);
 
-    /**
-     * @brief Push one element into the queue.
-     * @note Its time complexity shall be O(log n).
-     */
+    /** Adds one element to the queue. */
     void push(const T&);
 
     /**
-     * @return A const reference of the top element in the queue.
-     * @throws container_is_empty when the top element does not exist.
-     * @note Its time complexity shall be O(1).
+     * Returns a read-only reference of the first element in the queue.
+     *
+     * @throws container_is_empty when the first element does not exist.
      */
     const T& top() const;
 
     /**
-     * @brief remove the top element in the queue.
-     * @throws container_is_empty when the top element does not exist.
-     * @note Its time complexity shall be O(n).
+     * Removes the first element in the queue.
+     *
+     * @throws container_is_empty when the first element does not exist.
      */
     void pop();
 
-    /**
-     * @return number of elements in the queue.
-     */
+    /** Returns the number of elements in the queue. */
     size_t size() const;
 
-    /**
-     * @return whether there is any element in the queue.
-     */
+    /** Returns whether there is any element in the queue. */
     bool empty() const;
 
-    /**
-     * @brief Clear all elements in the queue.
-     * @note Its time complexity shall be O(n).
-     */
+    /** Clears all elements in the queue. */
     void clear();
 
     /**
-     * @brief Merge element sets of two queues.
-     * @note Its time complexity shall be O(log n).
+     * @brief Merges two priority queues into one.
+     *
+     * The merged data shall be stored in the current priority queue and the
+     * other priority queue shall be cleared after merging.
+     *
+     * The time complexity shall be O(log n) or better.
      */
     void merge(priority_queue&);
 };
